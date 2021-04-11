ba1f953367b743ee410cc7c9849d10d478b95759
simonpeterrasmussen <simon_peter_rasmussen@hotmail.com>
2021-04-11 09:49:52 +0200

    Correction of union

--- Eksamen/CTE_Roles.sql
+++ Eksamen/CTE_Roles.sql
@@ -4,7 +4,7 @@ Select privileges
 Select links      
 */
 USE MXMC_db;
-WITH RoleTable (Id, RoleId, RoleName)
+WITH RoleTable (Id, RoleID, RoleName)
 	AS (
         SELECT a.mcMSKEY, a.mcMSKEYVALUE AS 'RoleID', b.aValue AS 'RoleName'
             FROM idmv_entry_simple AS a WITH (nolock)
@@ -17,8 +17,8 @@ WITH RoleTable (Id, RoleId, RoleName)
         SELECT a.mcMSKEY, a.mcMSKEYVALUE AS 'PrivID', b.aValue AS 'PrivName', c.aValue AS 'PrivDescription', d.aValue AS 'RepositoryId'
             FROM idmv_entry_simple AS a WITH (nolock)
             LEFT OUTER JOIN idmv_value_basic_active AS b WITH (nolock) ON b.MSKEY = a.mcMSKEY AND (b.AttrName = 'DISPLAYNAME' OR b.Attr_ID IS NULL )
-            LEFT OUTER JOIN idmv_value_basic_active AS c WITH (nolock) ON c.MSKEY = a.mcMSKEY AND (c.AttrName = 'DESCRIPTION' OR c.Attr_ID IS NULL )
-            LEFT OUTER JOIN idmv_value_basic_active AS d WITH (nolock) ON d.MSKEY = a.mcMSKEY AND (d.AttrName = 'MX_REPOSITORYNAME' OR d.Attr_ID IS NULL )
+            LEFT OUTER JOIN idmv_value_basic_active AS c WITH (nolock) ON c.MSKEY = a.mcMSKEY AND (c.AttrName = 'DISPLAYNAME' OR c.Attr_ID IS NULL )
+            LEFT OUTER JOIN idmv_value_basic_active AS d WITH (nolock) ON d.MSKEY = a.mcMSKEY AND (d.AttrName = 'DISPLAYNAME' OR d.Attr_ID IS NULL )
             WHERE a.mcEntryType = 'MX_PRIVILEGE'
                 AND mcIdStore = 1
     ),
@@ -35,24 +35,18 @@ WITH RoleTable (Id, RoleId, RoleName)
             FROM idmv_link_simple_active WITH (nolock)
             WHERE mcThisEntryType = 9
                 AND mcOtherEntryType = 10
-    ),
-    UserPrivTable (UserId, PrivId)
-    AS (
-        SELECT CAST(mcThisMSKEY AS nvarchar),  CAST(mcOtherMSKEY AS nvarchar)
-            FROM idmv_link_simple_active WITH (nolock)
-            WHERE mcThisEntryType = 7
-                AND mcOtherEntryType = 9        
-    ),
-    UserRoleTable (UserId, RoleId)
-    AS (
-        SELECT  CAST(mcThisMSKEY AS nvarchar),  CAST(mcOtherMSKEY AS nvarchar)--,  mcThisEntryType, mcOtherEntryType
-            FROM idmv_link_simple_active WITH (nolock)
-            WHERE mcThisEntryType = 7
-                AND mcOtherEntryType = 10        
     )
 
-SELECT r.RoleId, r.RoleName, rt.RoleID AS 'SubRoleId', rt.RoleName AS 'SubRoleName'
+SELECT r.RoleID, r.RoleName, rt.RoleID AS 'Role/PrivId', rt.RoleName AS 'Role/PrivName'
     FROM RoleTable AS r 
-    JOIN RoleRoleTable AS rr ON rr.RoleToId = r.ID
-    JOIN RoleTable AS rt ON rt.Id = rr.RoleFromId
-    WHERE r.RoleId LIKE 'BR:GBR_L%00000000_20_GMA%';
+    LEFT OUTER JOIN RoleRoleTable AS rr ON rr.RoleToId = r.ID
+    LEFT OUTER JOIN RoleTable AS rt ON rt.Id = rr.RoleFromId
+    WHERE r.RoleId LIKE 'BR:GBR_L%00000000_20_GMA%'
+
+UNION ALL
+
+SELECT r.RoleID, r.RoleName, p.PrivID, p.PrivName
+    FROM RoleTable AS r 
+    LEFT OUTER JOIN PrivRoleTable AS pr ON pr.RoleId = r.ID
+    LEFT OUTER JOIN PrivilegesTable AS p ON p.Id = pr.PrivId
+    WHERE r.RoleId LIKE 'BR:GBR_L%00000000_20_GMA%';    
